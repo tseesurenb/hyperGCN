07e4cbe094aebb0b59d0055da64c704934d57643
tseesurenb <133945855+tseesurenb@users.noreply.github.com>
2024-08-27 10:18:55 +1000

    hi

--- main.py
+++ main.py
@@ -119,7 +119,7 @@ else:
 
 print(f'\n-------------------------------------------- Experimental Results --------------------------------------------')    
 print(f"Dataset: {config['dataset']}, num_users: {num_users}, num_items: {num_items}, num_interactions: {num_interactions}")
-print(f"MODEL: {br}{config['model']} | EDGE TYPE: {config['edge']} | EMB_DIM: {config['emb_dim']} | #LAYERS: {config['layers']} | SIM: u-{config['u_sim']}(topK {config['u_sim_top_k']}), i-{config['i_sim']}(topK {config['i_sim_top_k']}) | Self-sim: {config['self_sim']} | BATCH_SIZE: {config['batch_size']} | DECAY: {config['decay']} | EPOCHS: {config['epochs']}")
+print(f"MODEL: {br}{config['model']}{rs} | EDGE TYPE: {br}{config['edge']}{rs} | EMB_DIM: {br}{config['emb_dim']}{rs} | #LAYERS: {br}{config['layers']}{rs} | SIM: {br}u-{config['u_sim']}(topK {config['u_sim_top_k']}), i-{config['i_sim']}(topK {config['i_sim_top_k']}){rs} | Self-sim: {br}{config['self_sim']}{rs} | BATCH_SIZE: {config['batch_size']} | DECAY: {br}{config['decay']}{rs} | EPOCHS: {br}{config['epochs']}{rs}")
 print(f"  Recall: {recalls[0]:.4f}, {recalls[1]:.4f}, {recalls[2]:.4f}, {recalls[3]:.4f}, {recalls[4]:.4f} | {round(np.mean(recalls), 4):.4f}, {round(np.std(recalls), 4):.4f}")
 print(f"    Prec: {precs[0]:.4f}, {precs[1]:.4f}, {precs[2]:.4f}, {precs[3]:.4f}, {precs[4]:.4f} | {round(np.mean(precs), 4):.4f}, {round(np.std(precs), 4):.4f}")
 print(f"F1 score: {f1s[0]:.4f}, {f1s[1]:.4f}, {f1s[2]:.4f}, {f1s[3]:.4f}, {f1s[4]:.4f} | {round(np.mean(f1s), 4):.4f}, {round(np.std(f1s), 4):.4f}")
