3e7d9948854d3ace40be8e8d3180fcc8f3300ec6
tseesurenb <133945855+tseesurenb@users.noreply.github.com>
2024-08-30 11:37:19 +1000

    hi

--- utils.py
+++ utils.py
@@ -110,7 +110,7 @@ def get_metrics(user_Embed_wts, item_Embed_wts, n_users, n_items, train_data, te
 
     return metrics_df['recall'].mean(), metrics_df['precision'].mean(), metrics_df['ndcg'].mean()
 
-def batch_data_loader(data, batch_size, n_usr, n_itm, device):
+def batch_data_loader_old(data, batch_size, n_usr, n_itm, device):
 
     def sample_neg(x):
         while True:
@@ -132,6 +132,35 @@ def batch_data_loader(data, batch_size, n_usr, n_itm, device):
     interacted_items_df = pd.merge(interacted_items_df, users_df, how = 'right', left_on = 'user_id_idx', right_on = 'users')
     pos_items = interacted_items_df['item_id_idx'].apply(lambda x : random.choice(x)).values
     neg_items = interacted_items_df['item_id_idx'].apply(lambda x: sample_neg(x)).values
+
+    return (
+        torch.LongTensor(list(users)).to(device), 
+        torch.LongTensor(list(pos_items)).to(device) + n_usr,
+        torch.LongTensor(list(neg_items)).to(device) + n_usr
+    )
+    
+def batch_data_loader(data, batch_size, n_usr, n_itm, device):
+
+    def sample_neg(x):
+        while True:
+            neg_id = random.randint(0, n_itm - 1)
+            if neg_id not in x:
+                return neg_id
+
+    interacted_items_df = data.groupby('user_id')['item_id'].apply(list).reset_index()
+    indices = [x for x in range(n_usr)]
+
+    if n_usr < batch_size:
+        users = [random.choice(indices) for _ in range(batch_size)]
+    else:
+        users = random.sample(indices, batch_size)
+        
+    users.sort()
+    users_df = pd.DataFrame(users,columns = ['users'])
+
+    interacted_items_df = pd.merge(interacted_items_df, users_df, how = 'right', left_on = 'user_id', right_on = 'users')
+    pos_items = interacted_items_df['item_id'].apply(lambda x : random.choice(x)).values
+    neg_items = interacted_items_df['item_id'].apply(lambda x: sample_neg(x)).values
 
     return (
         torch.LongTensor(list(users)).to(device), 
